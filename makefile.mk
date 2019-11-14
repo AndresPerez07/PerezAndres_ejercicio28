7a96dea4931bac8df715ba51738bb6a97afb3d57
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-14 13:45:29 -0500

    Add files via upload

--- makefile.mk
+++ makefile.mk
@@ -1,9 +1,9 @@
 Proyectil_rk4.png : rk4.dat plot_rk4.py
-						python plot_rk4.py
+						 python plot_rk4.py
 rk4.dat : proyectil_rk4.x
    ./proyectil_rk4.x
 proyectil_rk4.x : proyectil_rk4.cpp
-	         c++ proyectil_rk4.cpp o- proyectil_rk4.x
+	         c++ proyectil_rk4.cpp -o proyectil_rk4.x
 
 clean :
 	  rm rk4.dat proyectil_rk4.x proyectil_rk4.png
